f48c933d8cd3f25b5937e2d4edff2ffe28c0b412
Richard Gao <coolrichardg54@gmail.com>
2020-05-28 19:20:20 -0700

    Updates

---
+++ precache-manifest.73f2f36e7925356df332ce50172befd9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ab89574386aa4372d18a0c09f272b4",
+    "url": "/Compass-Mentors/index.html"
+  },
+  {
+    "revision": "f35f4c407a7adbc51c76",
+    "url": "/Compass-Mentors/static/css/main.dffa62cd.chunk.css"
+  },
+  {
+    "revision": "b6a4b997f08546045fff",
+    "url": "/Compass-Mentors/static/js/2.9bfd962e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Compass-Mentors/static/js/2.9bfd962e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f35f4c407a7adbc51c76",
+    "url": "/Compass-Mentors/static/js/main.0fad5467.chunk.js"
+  },
+  {
+    "revision": "8d443cb125fca75a98ad",
+    "url": "/Compass-Mentors/static/js/runtime-main.05785c68.js"
+  },
+  {
+    "revision": "d14035f6b1afeabafbee4abb2ebf0fc6",
+    "url": "/Compass-Mentors/static/media/Cocogoose_trial.d14035f6.otf"
+  },
+  {
+    "revision": "eac5f1c85ca6675aa868d31a23799d76",
+    "url": "/Compass-Mentors/static/media/Example.eac5f1c8.png"
+  },
+  {
+    "revision": "4c578dd71909b2503b900e1f1fbf4f0f",
+    "url": "/Compass-Mentors/static/media/arrow-ios-forward-outline.4c578dd7.svg"
+  },
+  {
+    "revision": "a31f5edceeeddba1549e74f0171c1a1c",
+    "url": "/Compass-Mentors/static/media/close-outline.a31f5edc.svg"
+  },
+  {
+    "revision": "dc2f6cafbf7b86fd5421d3b531deae1d",
+    "url": "/Compass-Mentors/static/media/compass-mentors-graphic.dc2f6caf.png"
+  },
+  {
+    "revision": "b18eadddea1de90c841fdfa58641c1c0",
+    "url": "/Compass-Mentors/static/media/expand-menu.b18eaddd.svg"
+  }
+]);
